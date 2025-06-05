7652e6ac04327350b826244be382b8eba0bf6b8a
Rohit <mohiter852@gmail.com>
2025-06-05 12:44:29 +0530

    added Wishlist.jsx

---
+++ src/pages/Ecommerce/Wishlist.jsx
@@ -0,0 +1,220 @@
+import React from "react";
+import Header from "./Header";
+import { IoClose } from "react-icons/io5";
+
+function Wishlist() {
+  return (
+    <div>
+      {/* <Header /> */}
+      <section className="container">
+        <div className=" font-Poppins py-10">
+          <h6 className="capitalize text-xl text-[#334A78]">
+            my wishlist <span className="text-[#334A78]/70">15 items</span>
+          </h6>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6">
+          <div className="font-Poppins w-[245px] h-[350px] relative">
+            <div className="flex justify-center items-center p-2">
+              <img
+                src="/images/home/product-image.png"
+                alt="chair"
+                className="h-52 object-contain"
+              />
+            </div>
+            <div className="bg-[#fff] p-2">
+              <div className="flex mb-4">
+                <div className="flex-1 text-sm  leading-[22.4px]  text-[#111] space-y-1.5">
+                  <h4 className="font-medium text-sm leading-[22.4px] uppercase">
+                    FLAMINGO SLING
+                  </h4>
+                  <div className="flex items-center gap-2">
+                    <p className=" ">Rs 3,0000</p>
+                    <p className="line-through text-[#111] text-opacity-50">
+                      Rs $5678
+                    </p>
+                    <p className="text-[#C20000] uppercase">sale</p>
+                  </div>
+                </div>
+              </div>
+              <button className="text-[#000] capitalize bg-[#FFFFFF] text-xs border border-[#ccc] px-2  py-2 rounded-sm ">
+                move to cart
+              </button>
+            </div>
+            <div className="absolute top-2 right-2">
+              <button className="bg-[#ffffff]/30 p-1 rounded-full">
+                <IoClose />
+              </button>
+            </div>
+          </div>
+          <div className="font-Poppins w-[245px] h-[350px] relative">
+            <div className="flex justify-center items-center p-2">
+              <img
+                src="/images/home/product-image.png"
+                alt="chair"
+                className="h-52 object-contain"
+              />
+            </div>
+            <div className="bg-[#fff] p-2">
+              <div className="flex mb-4">
+                <div className="flex-1 text-sm  leading-[22.4px]  text-[#111] space-y-1.5">
+                  <h4 className="font-medium text-sm leading-[22.4px] uppercase">
+                    FLAMINGO SLING
+                  </h4>
+                  <div className="flex items-center gap-2">
+                    <p className=" ">Rs 3,0000</p>
+                    <p className="line-through text-[#111] text-opacity-50">
+                      Rs $5678
+                    </p>
+                    <p className="text-[#C20000] uppercase">sale</p>
+                  </div>
+                </div>
+              </div>
+              <button className="text-[#000] capitalize bg-[#FFFFFF] text-xs border border-[#ccc] px-2  py-2 rounded-sm ">
+                move to cart
+              </button>
+            </div>
+            <div className="absolute top-2 right-2">
+              <button className="bg-[#ffffff]/30 p-1 rounded-full">
+                <IoClose />
+              </button>
+            </div>
+          </div>
+          <div className="font-Poppins w-[245px] h-[350px] relative">
+            <div className="flex justify-center items-center p-2">
+              <img
+                src="/images/home/product-image.png"
+                alt="chair"
+                className="h-52 object-contain"
+              />
+            </div>
+            <div className="bg-[#fff] p-2">
+              <div className="flex mb-4">
+                <div className="flex-1 text-sm  leading-[22.4px]  text-[#111] space-y-1.5">
+                  <h4 className="font-medium text-sm leading-[22.4px] uppercase">
+                    FLAMINGO SLING
+                  </h4>
+                  <div className="flex items-center gap-2">
+                    <p className=" ">Rs 3,0000</p>
+                    <p className="line-through text-[#111] text-opacity-50">
+                      Rs $5678
+                    </p>
+                    <p className="text-[#C20000] uppercase">sale</p>
+                  </div>
+                </div>
+              </div>
+              <button className="text-[#000] capitalize bg-[#FFFFFF] text-xs border border-[#ccc] px-2  py-2 rounded-sm ">
+                move to cart
+              </button>
+            </div>
+            <div className="absolute top-2 right-2">
+              <button className="bg-[#ffffff]/30 p-1 rounded-full">
+                <IoClose />
+              </button>
+            </div>
+          </div>
+          <div className="font-Poppins w-[245px] h-[350px] relative">
+            <div className="flex justify-center items-center p-2">
+              <img
+                src="/images/home/product-image.png"
+                alt="chair"
+                className="h-52 object-contain"
+              />
+            </div>
+            <div className="bg-[#fff] p-2">
+              <div className="flex mb-4">
+                <div className="flex-1 text-sm  leading-[22.4px]  text-[#111] space-y-1.5">
+                  <h4 className="font-medium text-sm leading-[22.4px] uppercase">
+                    FLAMINGO SLING
+                  </h4>
+                  <div className="flex items-center gap-2">
+                    <p className=" ">Rs 3,0000</p>
+                    <p className="line-through text-[#111] text-opacity-50">
+                      Rs $5678
+                    </p>
+                    <p className="text-[#C20000] uppercase">sale</p>
+                  </div>
+                </div>
+              </div>
+              <button className="text-[#000] capitalize bg-[#FFFFFF] text-xs border border-[#ccc] px-2  py-2 rounded-sm ">
+                move to cart
+              </button>
+            </div>
+            <div className="absolute top-2 right-2">
+              <button className="bg-[#ffffff]/30 p-1 rounded-full">
+                <IoClose />
+              </button>
+            </div>
+          </div>{" "}
+          <div className="font-Poppins w-[245px] h-[350px] relative">
+            <div className="flex justify-center items-center p-2">
+              <img
+                src="/images/home/product-image.png"
+                alt="chair"
+                className="h-52 object-contain"
+              />
+            </div>
+            <div className="bg-[#fff] p-2">
+              <div className="flex mb-4">
+                <div className="flex-1 text-sm  leading-[22.4px]  text-[#111] space-y-1.5">
+                  <h4 className="font-medium text-sm leading-[22.4px] uppercase">
+                    FLAMINGO SLING
+                  </h4>
+                  <div className="flex items-center gap-2">
+                    <p className=" ">Rs 3,0000</p>
+                    <p className="line-through text-[#111] text-opacity-50">
+                      Rs $5678
+                    </p>
+                    <p className="text-[#C20000] uppercase">sale</p>
+                  </div>
+                </div>
+              </div>
+              <button className="text-[#000] capitalize bg-[#FFFFFF] text-xs border border-[#ccc] px-2  py-2 rounded-sm ">
+                move to cart
+              </button>
+            </div>
+            <div className="absolute top-2 right-2">
+              <button className="bg-[#ffffff]/30 p-1 rounded-full">
+                <IoClose />
+              </button>
+            </div>
+          </div>{" "}
+          <div className="font-Poppins w-[245px] h-[350px] relative">
+            <div className="flex justify-center items-center p-2">
+              <img
+                src="/images/home/product-image.png"
+                alt="chair"
+                className="h-52 object-contain"
+              />
+            </div>
+            <div className="bg-[#fff] p-2">
+              <div className="flex mb-4">
+                <div className="flex-1 text-sm  leading-[22.4px]  text-[#111] space-y-1.5">
+                  <h4 className="font-medium text-sm leading-[22.4px] uppercase">
+                    FLAMINGO SLING
+                  </h4>
+                  <div className="flex items-center gap-2">
+                    <p className=" ">Rs 3,0000</p>
+                    <p className="line-through text-[#111] text-opacity-50">
+                      Rs $5678
+                    </p>
+                    <p className="text-[#C20000] uppercase">sale</p>
+                  </div>
+                </div>
+              </div>
+              <button className="text-[#000] capitalize bg-[#FFFFFF] text-xs border border-[#ccc] px-2  py-2 rounded-sm ">
+                move to cart
+              </button>
+            </div>
+            <div className="absolute top-2 right-2">
+              <button className="bg-[#ffffff]/30 p-1 rounded-full">
+                <IoClose />
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Wishlist;
